cfc75b76961b8a9a348ea83a4ad960b947b63b47
Younwoo Ki <ts06067@hanmail.net>
2023-11-24 14:30:09 -0500

    update

--- MakeClusters.py
+++ MakeClusters.py
@@ -12,13 +12,13 @@ from gerrychain import (
     Election,
     graph,
 )
-from gerrychain.proposals import recom
-from functools import partial
 import pandas as pd
 import geopandas as gpd
-from tqdm import tqdm
 import numpy as np
+
 from sklearn.manifold import MDS
+from sklearn.cluster import KMeans
+
 from datetime import datetime
 from os import makedirs
 
@@ -31,7 +31,7 @@ from random import random
 
 MAX_NUM_PLANS = 1000
 MAX_NUM_COMPARISON = MAX_NUM_PLANS * MAX_NUM_PLANS / 2
-NUM_PLANS = 5
+NUM_PLANS = 100
 NUM_COMPARISON = NUM_PLANS * NUM_PLANS / 2
 NUM_CORES = 4  # 16
 NUM_COMPARISON_PER_CORE = math.ceil(NUM_COMPARISON / NUM_CORES)
@@ -86,38 +86,6 @@ def makeCluster(arg):
 
     return distance_matrix
 
-    """
-    n = 9  # number of plans
-
-    distances = np.zeros((n, n))
-
-    # configure plans
-    plans = []
-    for i in range(2000, 11000, 1000):
-        districts = gpd.read_file(f"./units/az_pl2020_vtd_{i}.json")
-        graph = Graph.from_geodataframe(districts, ignore_errors=True)
-        plans.append(GeographicPartition(graph, assignment="SLDL_DIST"))
-
-    for outer_idx, outer_plan in tqdm(enumerate(plans)):
-        for inner_idx in range(outer_idx + 1, n):
-            print("inner_idx: ", inner_idx)
-            inner_plan = plans[inner_idx]
-            distances[outer_idx, inner_idx] = Pair(outer_plan, inner_plan).distance
-
-    print(distances)
-
-    mds = MDS(n_components=2, random_state=0, dissimilarity="precomputed")
-    pos = mds.fit(distances).embedding_
-
-    # save image
-    plt.scatter(pos[:, 0], pos[:, 1])
-    plt.savefig("./clusters/mds.png")
-
-    end_time = datetime.now()
-
-    print("Duration: ", end_time - start_time)
-    """
-
 
 if __name__ == "__main__":
     makedirs("clusters", exist_ok=True)
@@ -149,4 +117,48 @@ if __name__ == "__main__":
     mds = MDS(n_components=2, random_state=0, dissimilarity="precomputed")
     pos = mds.fit(distances).embedding_
 
+    # use this for k-means clustering
     print(pos)
+
+    # using pos, run k-means clustering with elbow method to find optimal number of clusters
+    distortions = []
+    K = range(1, NUM_PLANS)
+    for k in K:
+        kmeanModel = KMeans(n_clusters=k)
+        kmeanModel.fit(pos)
+        distortions.append(kmeanModel.inertia_)
+    plt.figure(figsize=(16, 8))
+    plt.plot(K, distortions, "bx-")
+    plt.xlabel("k")
+    plt.ylabel("Distortion")
+    plt.title("The Elbow Method showing the optimal k")
+    plt.savefig("./clusters/elbow.png")
+    plt.close()
+
+    print("distortions:", distortions)
+
+    # calculate elbow point
+    elbow = 0
+    for i in range(len(distortions) - 1):
+        print("i:", i)
+        print(abs(distortions[i + 1] - distortions[i]))
+        if abs(distortions[i + 1] - distortions[i]) < 1:
+            elbow = i + 1
+            break
+
+    print("elbow:", elbow)
+
+    # perform k-means clustering with elbow point based on pos
+    kmeans = KMeans(n_clusters=elbow)
+    kmeans.fit(pos)
+    y_kmeans = kmeans.predict(pos)
+
+    print("y_kmeans:", y_kmeans)
+    print("kmeans.cluster_centers_:", kmeans.cluster_centers_)
+
+    # plot clusters
+    plt.scatter(pos[:, 0], pos[:, 1], c=y_kmeans, s=50, cmap="viridis")
+    centers = kmeans.cluster_centers_
+    plt.scatter(centers[:, 0], centers[:, 1], c="black", s=200, alpha=0.5)
+    plt.savefig("./clusters/clusters.png")
+    plt.close()
